99a507c5c9a0fd7b64a0ceb8aedafcf1a865c7c8
Betolann <betolanp@hotmail.com>
2015-09-09 16:06:20 -0600

    Agregue métodos.

--- src/pandoraunderattack/Reliquias.java
+++ src/pandoraunderattack/Reliquias.java
@@ -1,9 +1,6 @@
 
 package pandoraunderattack;
 
-
-
-
 public class Reliquias {
     
     String NombreReliquia;
@@ -17,8 +14,67 @@ public class Reliquias {
     public void AddReliquias(){
         
         DLList reliquias = new DLList();
-        reliquias.insertarInicio(NombreReliquia);
-       
-        
+        reliquias.insertarInicio(NombreReliquia);   
     }
+
+
+
+	public String getNombreReliquia() {
+		return this.NombreReliquia;
+	}
+
+
+
+	public void setNombreReliquia(String nombreReliquia) {
+		this.NombreReliquia = nombreReliquia;
+	}
+
+
+
+	public int getReliquias() {
+		return this.reliquias;
+	}
+
+
+
+	public void setReliquias(int reliquias) {
+		this.reliquias = reliquias;
+	}
+
+
+
+	public int getPeso() {
+		return this.peso;
+	}
+
+
+
+	public void setPeso(int peso) {
+		this.peso = peso;
+	}
+
+
+
+	public int getX() {
+		return this.x;
+	}
+
+
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+
+
+	public int getY() {
+		return this.y;
+	}
+
+
+
+	public void setY(int y) {
+		this.y = y;
+	}
+    
 }
